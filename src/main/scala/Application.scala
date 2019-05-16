e7e9509cb45bb7a20e2a75e0bfa396653804b480
柏木　裕介 <191x205x@istc-b-050.epc.center.kobe-u.ac.jp>
2019-05-16 12:20:58 +0900

    5/16

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -5,9 +5,11 @@
 object Application extends App {
   val x1 = Variable("x1")
   val x2 = Variable("x2")
+  val x3 = Variable("x3")
 
   val dom1 = Domain(Seq(1,2,3))
   val dom2 = Domain(Seq(1, 2, 3))
+  val dom3 = Domain(Seq(1, 4, 4))
 
   val c1 = Ne(x1, x2)
 
@@ -23,4 +25,50 @@ object Application extends App {
       */
   }
 
+  val csp2 = CSP(Seq(x1, x2, x3), Map(x1 -> dom1, x2 -> dom2, x3 -> dom3), ad(Seq(x1, x2, x3)))
+
+  //csp.toSugar.foreach(println)
+
+
+}
+
+object Test extends App{
+
+  gt01
+
+
+  def gt01 = {
+    val x1 = Variable("x1")
+    val x2 = Variable("x2")
+    val x3 = Variable("x3")
+
+    val vars = Seq(x1,x2,x3)
+
+    val doms =
+      Seq(
+        x1 -> Domain(Seq(1, 2, 3)),
+        x2 -> Domain(Seq(1, 2, 3)),
+        x3 -> Domain(Seq(1, 2, 3))).toMap
+
+    val cons =
+      Seq(
+        Ne(x1, x2),
+        Ne(x1, x3),
+        Ne(x2, x3))
+
+    val csp = new CSP(vars, doms, cons)
+
+    val solver = new GT
+
+    val solution = solver.solve(csp)
+
+
+    if (solution.nonEmpty) {
+      println("s SAT")
+      println(solution.get)
+    } else {
+      println("s UNSAT")
+    }
+
+  }
 }
--- src/main/scala/CSP.scala
+++ src/main/scala/CSP.scala
@@ -4,9 +4,15 @@
  */
 abstract class Expression
 
-abstract class Term extends Expression
+abstract class Term extends Expression {
+  def vars: Set[Variable]
+  def valuedWith(a: Assignment): Int
+}
 
-case class Variable(name: String) extends Term
+case class Variable(name: String) extends Term {
+  def vars = Set(this)
+  def valuedWith(a: Assignment) = a(this)
+}
 
 case class Domain(values: Seq[Int]) extends Expression{
   def lb = values.min
@@ -31,11 +37,19 @@ case class CSP(
 
   lazy val var2cons = (for (x <- vars) yield x -> cons.filter(_.vars.contains(x))).toMap
 
+  /* ==========================
+     修正ここから
+     ==========================
+   */
   def toSugar(t: Expression): String = t match {
     case x: Variable => s"(int ${x.name} ${toSugar(doms(x))})"
     case d: Domain => if (d.values.size == d.ub - d.lb + 1) s"${d.lb} ${d.ub}" else d.values.mkString(" ")
-    case c: Ne => s"(ne ${c.x1.name} ${c.x2.name})"
+    case Ne(x1: Variable, x2: Variable) => s"(ne ${x1.name} ${x2.name})"
   }
+  /* ==========================
+     修正ここまで
+     ==========================
+   */
 
   def toSugar: Seq[String] = {
     vars.map(toSugar(_)) ++ cons.map(toSugar(_))
@@ -56,6 +70,18 @@ case class Assignment(amap: Map[Variable, Int]) {
   def +(xv: Tuple2[Variable, Int]) = Assignment(amap + (xv._1 -> xv._2))
 
   def toDoms: Map[Variable, Domain] = amap.map { xv => xv._1 -> Domain(Seq(xv._2)) }
+
+  /* ==========================
+     修正ここから
+     ==========================
+   */
+  override def toString = {
+    amap.map{case (x, v) => s"v ${x.name} = $v"}.mkString("\n")
+  }
+  /* ==========================
+     修正ここまで
+     ==========================
+   */
 }
 
 object Assignment{
@@ -66,14 +92,58 @@ object Assignment{
   * @param x1
   * @param x2
   */
-case class Ne(x1: Variable, x2: Variable) extends Constraint{
-  def vars = Set(x1, x2)
-  def isSatisfiedWith(a: Assignment) = a(x1) != a(x2)
+
+case class Add(ts: Seq[Term]) extends Term{
+  def vars = ???
+  def valuedWith(a:Assignment): Int = ???
+}
+
+case class Ne(x1: Term, x2: Term) extends Constraint{
+  def vars = x1.vars ++ x2.vars
+  def isSatisfiedWith(a: Assignment) = x1.valuedWith(a) != x2.valuedWith(a)
 }
 
 case class Eq(x1: Variable, x2: Variable) extends Constraint{
-  def vars = Set(x1, x2)
-  def isSatisfiedWith(a: Assignment) = a(x1) == a(x2)
+  def vars = x1.vars ++ x2.vars
+  def isSatisfiedWith(a: Assignment) = x1.valuedWith(a) == x2.valuedWith(a)
 }
 
+case class Ge(x1: Variable, x2: Variable) extends Constraint{
+  def vars = x1.vars ++ x2.vars
+  def isSatisfiedWith(a: Assignment) = x1.valuedWith(a) > x2.valuedWith(a)
+}
+
+
+
+object cspFactory {
+  private[this] def varFactory(x: SIntVar): Variable = Variable(x.name)
+  private[this] def domFactory(d: SDomain) = {
+    val ds = d.dom.foldLeft(Seq.empty[Int])((seq, lu) => seq ++ (lu._1 to lu._2))
+    Domain(ds)
+  }
+  private[this] def termFactory(t: SugarCspTerm): Term = {
+    t match {
+      case x: SIntVar => varFactory(x)
+      case t: SAdd => ???
+    }
+  }
+  private[this] def constraintFactory(c: SugarCspConstraint): Constraint = {
+    c match {
+      case SNe(t1: SIntVar, t2: SIntVar) => Ne(varFactory(t1), varFactory(t2))
+      case t: SAdd => ???
+    }
+  }
+  def fromFile(fileName: String): CSP = {
+    val csp = CSP()
+    val sp = new SugarCspLangParser
+    sp.parse(new java.io.File(fileName))
+    sp.domMap.keys.foreach { x0 =>
+      val x = varFactory(x0)
+      csp.vars = x +: csp.vars
+      csp.doms += x -> domFactory(sp.domMap(x0))
+    }
+    csp.cons = sp.scons.map(constraintFactory)
+    csp
+  }
+}
 
